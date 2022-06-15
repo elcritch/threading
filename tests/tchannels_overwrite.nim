c7de8a1b038a6f36b9cd1b32fd2c14de30a9ae85
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-06-14 19:07:18 -0700

    use unit tests

--- tests/tchannels_overwrite.nim
+++ tests/tchannels_overwrite.nim
@@ -4,28 +4,36 @@ discard """
 """
 
 import threading/channels
-import std/logging
+import std/[logging, unittest]
 
-var logger = newConsoleLogger(levelThreshold=lvlInfo)
+suite "testing Chan with overwrite mode":
+  var logger = newConsoleLogger(levelThreshold=lvlInfo)
+  
+  setup:
+    discard "run before each test"
+  
+  teardown:
+    discard "run after each test"
 
-var chan = newChan[string](elements = 4, overwrite = true)
+  test "basic overwrite tests":
+    # give up and stop if this fails
+    require(true)
+    var chan = newChan[string](elements = 4, overwrite = true)
 
-# This proc will be run in another thread using the threads module.
-for i in 1..10:
-  logger.log(lvlDebug, "adding more messages than fit: " & $i)
-  chan.send("msg" & $i)
+    # This proc will be run in another thread using the threads module.
+    for i in 1..10:
+      logger.log(lvlDebug, "adding more messages than fit: " & $i)
+      chan.send("msg" & $i)
 
-var messages: seq[string]
-var msg = ""
-while true:
-  if chan.tryRecv(msg):
-    messages.add move(msg)
-  else:
-    break
+    var messages: seq[string]
+    var msg = ""
+    while true:
+      if chan.tryRecv(msg):
+        messages.add move(msg)
+      else:
+        break
 
-logger.log(lvlDebug, "got messages: " & $messages)
+    logger.log(lvlDebug, "got messages: " & $messages)
+    check messages == @["msg7", "msg8", "msg9", "msg10"]
 
-doAssert messages == @["msg7", "msg8", "msg9", "msg10"]
-
-# doAssert dest == "Hello World!"
 
